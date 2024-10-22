42ab07cf806f5e3a89f8d4130eca8ca576f459f7
JensHoog <jens.hoogelander@hva.nl>
2024-10-22 11:34:52 +0200

    Update Jens.py

--- Jens.py
+++ Jens.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+# Set the correct path to your Parquet file
+parquet_file_path = '/content/drive/My Drive/Statisticscode/TaoYin_Item_features.parquet'
+
+# Load the Parquet file into a DataFrame
+df = pd.read_parquet(parquet_file_path)
+
+# Display the first few rows of the DataFrame
+df.head()
